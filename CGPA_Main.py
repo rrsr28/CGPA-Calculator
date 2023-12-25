556c47baaee8a6859dadf082dec9bf52e97414c8
rrsr28 <rr.sanjayram@gmail.com>
2023-12-25 15:59:11 +0530

    New feature try

--- CGPA_Main.py
+++ CGPA_Main.py
@@ -14,3 +14,51 @@ def cgpa():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+"""def calculate_CGPA(image_path):
+    import cv2
+    import pytesseract
+
+    image = cv2.imread(image_path)
+
+    # Convert the image to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Apply thresholding to get a binary image
+    _, binary = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
+
+    # Perform OCR using pytesseract
+    text = pytesseract.image_to_string(binary)
+
+    points = 0
+    credit_hours = 0
+
+    for line in text.split('\n'):
+        if 'CREDITS' in line:
+            credit_hours += int(line.split()[0])
+
+        if 'GRADE' in line:
+            grade = line.split()[0]
+            if grade == '0':
+                points += 10
+            elif grade == 'A+':
+                points += 9
+            elif grade == 'A':
+                points += 8
+            elif grade == 'B+':
+                points += 7
+            elif grade == 'B':
+                points += 6
+            elif grade == 'C':
+                points += 5
+
+    if credit_hours != 0:
+        CGPA = points / credit_hours
+    else:
+        CGPA = 0
+
+    return CGPA
+
+image_path = 'ImagePath''
+CGPA = calculate_CGPA(image_path)
+print(f'Your CGPA is: {CGPA}')"""
